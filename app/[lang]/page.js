68272b47953bacf408f972e530a9963c8201b7e8
orun-sunny <sunny.mukut@gmail.com>
2024-04-15 11:14:18 +0600

    added interncepting

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,8 +1,12 @@
-import { getDictionary } from "./disctionaries";
+import PhotoList from "@/components/PhotoList";
+import { getDictionary } from "./dictionaries";
 
 export default async function Home({params: {lang}}) {
   const dictionary = await getDictionary(lang);
+  // console.log(process.env.BASE_API_URL);
+  const response = await fetch(`${process.env.BASE_API_URL}/photos`);
+  const photos = await response.json()
   return (
-    <div>{dictionary.followers}</div>
+ <PhotoList photos={photos}/>
   );
 }
